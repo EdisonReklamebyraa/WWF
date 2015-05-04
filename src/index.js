b1fa8236d9d94dbb48df80fd8c449a69648f9011
Mario <Mario@edisonmenlo.no>
2015-05-04 10:05:33 +0200

    added lib

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-var _ = require('lodash');
-var path = require('path');
 
-var background = require(path.join(__dirname,'data','background.js'));
-var electricityMix = require(path.join(__dirname,'data','electricity_mix.js'));
-var user = require(path.join(__dirname,'data','user.js'));
+var WWF = require('./wwf.js');
+var wwf = new WWF();
 
-
-
-
-module.exports = WWF;
-
-function WWF() {
-
-
-}
-
-wwf.prototype = _.create(wwf.prototype, {
-
-
-
-});
+$(document).foundation();
---
+++ src/wwf.js
@@ -0,0 +1,20 @@
+var _ = require('lodash');
+var background = require('./data/background.js');
+var electricityMix = require('./data/electricity_mix.js');
+var user = require('./data/user.js');
+
+
+
+
+
+module.exports = WWF;
+
+function WWF() {
+    console.log("xx");
+}
+
+WWF.prototype = _.create(WWF.prototype, {
+
+
+
+});
