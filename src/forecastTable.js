fbdab67ee8f090aaf63ff1dcc2708948bc55eeac
Mario <Mario@edisonmenlo.no>
2015-05-06 13:28:06 +0200

    update

---
+++ src/forecastTable.js
@@ -0,0 +1,34 @@
+var Arbiter = require('arbiter-subpub');
+var _ = require("lodash");
+
+module.exports = ForecastTable;
+
+function ForecastTable(data) {
+
+    var self = this;
+
+    Arbiter.subscribe("changed/user",function(json) {
+        self.update();
+    } );
+
+}
+
+ForecastTable.prototype = _.create(
+
+
+    ForecastTable.prototype,
+    {
+        data: null,
+
+        updateUserData: function(json) {
+            this.data = json;
+        },
+
+        update: function(json) {
+
+            var BDhot = new Handsontable(document.getElementById('BackgroundData'), {
+                                           data: json.background
+                                       });
+
+        }
+    });
