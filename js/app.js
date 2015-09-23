f71823251d688c134e7959383861068f9c7ad6b0
Mario <Mario@edisonmenlo.no>
2015-09-23 17:02:23 +0200

    get it working

--- js/app.js
+++ js/app.js
@@ -1165,6 +1165,7 @@ UIs.prototype = _.create(
             $("input[name=totalFund]").val(numeral(this.user["investment"] ).format('$ 0, 000')  );
             $("input[name=endYear]").val(this.user["target year"] );
             $("input[name=investPercentage]").val(numeral(this.user["target"] ).format('0%'));
+            $("input[name=growthRate]").val(numeral(this.user["annual growth rate"] ).format('0%'));
         },
 
         loadEvents: function(e) {
@@ -1174,8 +1175,8 @@ UIs.prototype = _.create(
                 var p = $(this).parent();
                 var t = $("input[name="+p.data("target")+"]");
                 var val = Math.max(numeral().unformat(p.data("min")) ,numeral().unformat(t.val()) - p.data("inc"));
-                t.val(  numeral(val).format(p.data("format")) );
-                self.interaction();
+                t.val( val );
+                self.interaction(t);
             });
 
             $(".upndownBox .upBtn").click(function(e) {
@@ -1183,25 +1184,24 @@ UIs.prototype = _.create(
                 var p = $(this).parent();
                 var t = $("input[name="+p.data("target")+"]");
                 var val = Math.max(numeral().unformat(p.data("min")) , numeral().unformat(t.val()) + p.data("inc"));
-                t.val(  numeral(val).format(p.data("format") ) );
-                self.interaction();
+                t.val( val );
+                self.interaction(t);
             });
 
             $("input").focusout(function() {
-                self.interaction();
+                self.interaction(this);
             }).change(function() {
-                self.interaction();
+                self.interaction(this);
             }).focus(function() {
                 if(!$(this).hasClass("string"))
                   $(this).val(numeral().unformat($(this).val()));
             });
         },
 
-        interaction: function() {
-            this.user["starting year"] = Math.max(2013,numeral().unformat($("input[name=startYear]").val()));
-            this.user["target year"] = Math.max(2013,this.user["starting year"] + 1 ,numeral().unformat($("input[name=endYear]").val()));
-            this.user["target"] = numeral().unformat($("input[name=investPercentage]").val());
-            this.user["investment"] = numeral().unformat($("input[name=totalFund]").val());
+        interaction: function(input) {
+
+            this.user[ $(input).data("id")] = $(input).val();
+
             Arbiter.publish("changed/user",this.user);
         }
 
